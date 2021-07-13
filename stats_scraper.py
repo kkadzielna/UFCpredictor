efa3b77067daf5ea44cbe5961dd17b06d408e419
kkadzielna <k.kadzielawa00@gmail.com>
2021-07-13 15:41:28 +0100

    stats retrieved, clean and put into a csv file

--- stats_scraper.py
+++ stats_scraper.py
@@ -13,11 +13,36 @@ for letter in alphabet:
     page = requests.get(url)
     soup = BeautifulSoup(page.text, 'html.parser')
 
-    fighterurl = []
+    fighterData = []
+    fighterurls = []
     for link in soup.find_all('a'):
         if link.get('href'):
             if "fighter-details" in link.get('href'):
-                fighterurl.append(link.get('href'))
-    fighterurl = list( dict.fromkeys(fighterurl) )
-    print(fighterurl)
+                fighterurls.append(link.get('href'))
+    fighterurls = list( dict.fromkeys(fighterurls) )
+    for i in fighterurls:
+        fighterurl = i
+        fighterpage = requests.get(fighterurl)
+        fightersoup = BeautifulSoup(fighterpage.text, 'html.parser')
+        #so maybe make columns in the csv file and fill them based on the titles of the li blocks?
+        """
+        for info in fightersoup.find_all('li'):
+            #info.get_text().remove('/n')
+            fighter = info.text.replace("\n", "").replace("', '", "").replace("    ", "")
+            fighterData.append(fighter)
+            #fighterData.append(info.contents)
+        print(fighterData)
+        """
+        for fighter in fightersoup.find_all('h2'):
+            #info.get_text().remove('/n') 'class="b-content__title-highlight"'
+            name = fighter.text.replace("   ", "").replace("\n", "")
+            print(name)
+            fighterData.append(name)
+            for info in fightersoup.find_all('li'):
+                stats = info.text.replace("\n", "").replace("', '", "").replace("    ", "")
+                print(stats)
+                fighterData.append(stats)
+        #print(fighterData)        
+
+    #now put that into a csv file
 
