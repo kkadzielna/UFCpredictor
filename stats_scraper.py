4a47bde12d725c9c48872d01d441ffddfbc97ec6
kkadzielna <k.kadzielawa00@gmail.com>
2021-08-31 23:40:12 +0100

    data scraped, usable format

--- stats_scraper.py
+++ stats_scraper.py
@@ -12,7 +12,6 @@ count = 0
 data = []
 alphabet = string.ascii_lowercase
 for letter in alphabet:
-    print(letter)
     url = "http://ufcstats.com/statistics/fighters?char=" + letter + "&page=all"
     page = requests.get(url)
     soup = BeautifulSoup(page.text, 'html.parser')
@@ -31,26 +30,38 @@ for letter in alphabet:
 
         for fighter in fightersoup.find_all('h2'):
             #'class="b-content__title-highlight"'
-            name = fighter.text.replace("   ", "").replace("\n", "")
+            name = fighter.text.replace("   ", "").replace("\n", "").replace("Record:", ",").strip()
             print(name)
-            #writerow name and record
-            fighterData.append(name)
+            nameAndRecord = name.split(",")
+            nameAndRecord[0] = nameAndRecord[0].strip()
+            nameAndRecord[1] = nameAndRecord[1].strip()
+
+            fighterData.append(nameAndRecord[0])
+            fighterData.append(nameAndRecord[1])
             for info in fightersoup.find_all('li'):
+
                 stats = info.text.replace("\n", "").replace("', '", "").replace("    ", "")
-                #writerow instead of appending, separate all the stats
+                stats = stats.replace("Height:", "")
+                stats = stats.replace("Weight:", "")
+                stats = stats.replace("Reach:", "")
+                stats = stats.replace("STANCE:", "")
+                stats = stats.replace("DOB:", "")
+                stats = stats.replace("SLpM:", "")
+                stats = stats.replace("Str. Acc.:", "")
+                stats = stats.replace("SApM:", "")
+                stats = stats.replace("Str. Def:", "")
+                stats = stats.replace("TD Avg.:", "")
+                stats = stats.replace("TD Acc.:", "")
+                stats = stats.replace("TD Def.:", "")
+                stats = stats.replace("Sub. Avg.:", "")
+                
                 print(stats)
                 fighterData.append(stats)
-        fighterData.append("\n")
-        #writerow fighterData
-        #i need to append the fighterData to something
+        fighterData = list(filter(None, fighterData))
         data.append(fighterData)
         csv_writer.writerow(fighterData)
         fighterData = []
 
-    #print(fighterData)   
-#fighterData.to_csv('fighters.csv')
-#gets only one fighter per letter for some reason
-#csv_writer.writerows(data)
 print(data)
-#now put that into a csv file
+
 
